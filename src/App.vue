e59b513d377adaa13af7511fd7edb5001d7e1cd7
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2024-02-27 11:38:41 +0700

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <script setup>
 import {RouterView } from 'vue-router'
-import HomePage from './components/HomePage.vue';
 </script>
 
 <template>
   <div>
-    <HomePage />
+    
   </div>
   <RouterView />
 </template>
