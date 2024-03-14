becad3db586a838209f08e282120bf116507a18e
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2024-03-14 11:30:01 +0700

    adding changes

--- src/components/Quotes.vue
+++ src/components/Quotes.vue
@@ -73,7 +73,10 @@ export default {
             disableOnInteraction: false,
         }" -->
     <div class="pb-4">
-        <swiper :slidesPerView="1" :spaceBetween="15" :modules="modules" class="mySwiper">
+        <swiper :slidesPerView="1" :autoplay="{
+            delay: 5000,
+            disableOnInteraction: false,
+        }" :spaceBetween="15" :modules="modules" class="mySwiper">
             <swiper-slide v-for="recite in received_quotes" class="rounded-lg">
                 <div class="flex flex-col p-8 w-[1440px]">
                     <div class="bg-[#FF914C] flex p-5 items-center justify-center h-[520px]">
@@ -82,7 +85,7 @@ export default {
                             <!-- <p class="text-5xl text-center text-black font-poppins font-semibold leading-[3.875rem] pb-4">
                                 </p> -->
                             <p class="text-base text-[40px] text-white font-poppins font-semibold leading-[62px]"> "{{
-                                recite.quote }}"  - {{ recite.author_name }} </p>
+                                recite.quote }}" - {{ recite.author_name }} </p>
                         </div>
 
                     </div>
