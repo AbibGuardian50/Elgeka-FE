caad7cd2537ebd9564113dc1735bb64a13243b0c
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2024-05-29 01:25:29 +0700

    ✨ feat:
    adding default pictures in infoRS

--- src/views/InfoRS.vue
+++ src/views/InfoRS.vue
@@ -1,3 +1,49 @@
+<template>
+    <Navbar />
+
+    <div>
+        <div class="h-screen text-[40px] leading-[62px] bg-hospital bg-no-repeat bg-center bg-cover max-h-[550px] flex flex-col justify-center items-center">
+            <p class="text-white text-center font-bold">RUMAH SAKIT REKOMENDASI</p>
+            <p class="text-white text-center font-bold">Komunitas ELGEKA Jawa Barat</p>
+        </div>
+
+        <div class="grid justify-center absolute top-[50rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center min-w-[320px] sm:min-w-[640px] md:min-w-[960px] lg:min-w-[1200px] max-w-[1260px] bg-gray-100">
+                <div v-for="data in paginatedInfoRS" :key="data.id" class="bg-verylightgray rounded-lg shadow-lg overflow-hidden w-[291px] h-[340px] flex flex-col justify-between">
+                    <div class="flex items-center justify-center pt-4">
+                        <img :src="data.image_url ? url + data.image_url : 'https://cdn.pixabay.com/photo/2021/11/20/03/16/doctor-6810750_1280.png'" alt="foto rumah sakit" class="w-full max-w-[262px] rounded-[1rem] max-h-[144px] object-cover">
+                    </div>
+                    <div class="p-4 flex flex-col justify-between flex-grow">
+                        <div>
+                            <h2 class="text-2xl font-bold text-gray-800 mb-2">{{ data.nama_rs }}</h2>
+                            <p class="text-gray-700 leading-tight">
+                                {{ data.lokasi_rs }}
+                            </p>
+                            <p class="text-gray-700 leading-tight mb-4">
+                                Nomor Telepon: {{ data.info_kontak }}
+                            </p>
+                        </div>
+                        <div class="flex justify-center">
+                            <a target="_blank" :href="data.link_maps">
+                                <button class="bg-teal text-white font-medium text-[10px] font-medium font-poppins leading-4 px-2 py-1 rounded-md">Google Maps</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- Pagination navigation -->
+            <div class="mt-4 flex justify-center">
+                <button @click="prevPage" :disabled="currentPage === 1" class="px-4 py-2 mr-2 bg-teal text-white rounded-md">Previous</button>
+                <button v-for="pageNumber in totalPages" :key="pageNumber" @click="goToPage(pageNumber)"
+                    :class="{ 'bg-teal text-white rounded-md': pageNumber === currentPage, 'bg-white text-blue-500 border border-blue-500 rounded-md': pageNumber !== currentPage }"
+                    class="px-4 py-2 mr-2">{{ pageNumber }}</button>
+                <button @click="nextPage" :disabled="currentPage === totalPages" class="px-4 py-2 bg-teal text-white rounded-md">Next</button>
+            </div>
+        </div>
+    </div>
+</template>
+
 <script>
 import Navbar from '../components/Navbar.vue'
 import axios from 'axios'
@@ -19,6 +65,7 @@ export default {
     },
     data() {
         return {
+            url: 'https://elgeka-web-api-production.up.railway.app/',
             InfoRS: [],
             paginatedInfoRS: [],
             perPage: 8, // Number of items per page
@@ -52,49 +99,6 @@ export default {
 };
 </script>
 
-<template>
-    <Navbar />
-
-    <div>
-        <div class="h-screen text-[40px] leading-[62px] bg-hospital bg-no-repeat bg-center bg-cover max-h-[550px] flex flex-col justify-center items-center">
-            <p class="text-white text-center font-bold">RUMAH SAKIT REKOMENDASI</p>
-            <p class="text-white text-center font-bold">Komunitas ELGEKA Jawa Barat</p>
-        </div>
-
-        <div class="grid justify-center absolute top-[50rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center min-w-[1200px] max-w-[1260px] bg-gray-100">
-                <div v-for="data in paginatedInfoRS" :key="data.id" class="bg-verylightgray rounded-lg shadow-lg overflow-hidden w-[291px] h-[327px] ">
-                    <div class="flex items-center justify-center pt-4">
-                        <img src="https://images.unsplash.com/photo-1454496522488-7a8e488e8606" alt="Mountain" class="w-full max-w-[262px] rounded-[1rem] max-h-[144px] object-cover">
-                    </div>
-                    <div class="p-4">
-                        <h2 class="text-2xl font-bold text-gray-800 mb-2">{{ data.nama_rs }}</h2>
-                        <p class="text-gray-700 leading-tight mb-4">
-                            {{ data.lokasi_rs }} Nomor Telepon: {{ data.info_kontak }}
-                        </p>
-                        <div class="flex justify-between items-center">
-                            <div class="flex items-center">
-                                <a target="_blank" :href="data.link_maps">
-                                    <button class="bg-teal text-white font-medium text-[10px] leading-4 px-2 py-1 rounded-md">Google Maps</button>
-                                </a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Pagination navigation -->
-            <div class="mt-4 flex justify-center">
-                <button @click="prevPage" :disabled="currentPage === 1" class="px-4 py-2 mr-2 bg-teal text-white rounded-md">Previous</button>
-                <button v-for="pageNumber in totalPages" :key="pageNumber" @click="goToPage(pageNumber)"
-                    :class="{ 'bg-teal text-white rounded-md': pageNumber === currentPage, 'bg-white text-blue-500 border border-blue-500 rounded-md': pageNumber !== currentPage }"
-                    class="px-4 py-2 mr-2">{{ pageNumber }}</button>
-                <button @click="nextPage" :disabled="currentPage === totalPages" class="px-4 py-2 bg-teal text-white rounded-md">Next</button>
-            </div>
-        </div>
-    </div>
-</template>
-
 <style scoped>
-/* Add your custom styles here */
+/* Tambahkan gaya custom Anda di sini */
 </style>
