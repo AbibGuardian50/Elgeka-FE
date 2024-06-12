27fb2207799e676d10e402eae5b0f44e2840b716
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2024-06-13 02:00:55 +0700

    🐞 fix: url redirect
    Fixing bug where link donation relative to origin website

--- src/views/Donasi.vue
+++ src/views/Donasi.vue
@@ -20,6 +20,14 @@ export default {
         } catch (error) {
             console.error(error);
         }
+    },
+    methods: {
+        ensureAbsoluteUrl(url) {
+            if (!url.match(/^https?:\/\//)) {
+                return 'http://' + url;
+            }
+            return url;
+        }
     }
 }
 </script>
@@ -31,12 +39,10 @@ export default {
             <p class="font-poppins font-bold max-md:text-[30px] text-[40px] text-teal text-center">{{ donasielgeka.data.title }}</p>
             <img class="max-w-[314px] max-h-[283px] border-8 border-teal my-4" :src="gambar_url + donasielgeka.data.image_url" alt="Gambar QR"
                 srcset="">
-            <a :href="donasielgeka.data.donate_link"
-                class="font-poppins font-bold text-[40px] max-md:text-[30px] text-center hover:underline mb-2" target="_blank">{{
-                    donasielgeka.data.donate_link }}</a>
+            <a :href="ensureAbsoluteUrl(donasielgeka.data.donate_link)"
+                class="font-poppins font-bold text-[40px] max-md:text-[30px] text-center hover:underline mb-2" target="_blank">
+                <button>{{ donasielgeka.data.donate_link }}</button></a>
             <p v-html="donasielgeka.data.content" class="font-poppins font-normal max-w-[673px] md:w-[673px] max-md:px-8 max-md:text-[14px] text-[#000000B2]"></p>
-
-
         </div>
     </div>
 </template>
