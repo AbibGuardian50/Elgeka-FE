1c6ce735e1bce06fbcfc22401c64303c462cae41
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2024-07-15 15:14:15 +0700

    ✨ feat: Show/Hide pass
    Adding new function to show and hide password in login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -17,6 +17,7 @@ export default {
             rememberedUsername: '',
             rememberedPassword: '',
             profiledata: '',
+            showPassword: false
         }
     },
     methods: {
@@ -46,9 +47,9 @@ export default {
                 if (error.response.data.ErrorMessage === "Invalid password") {
                     toast.error("Password salah")
                 } else if (error.response.data.ErrorMessage === "Invalid email or phone number") {
-                    toast.error ('Email atau password yang dimasukkan salah')
+                    toast.error('Email atau password yang dimasukkan salah')
                 } else {
-                    toast.error ('Terdapat kesalahan sistem, mohon coba lagi')
+                    toast.error('Terdapat kesalahan sistem, mohon coba lagi')
                 }
             }
         },
@@ -57,6 +58,9 @@ export default {
             expirationDate.setDate(expirationDate.getDate() + 30);
             // Mengatur cookie dengan nama "token" dan nilai token yang diberikan bersama dengan tanggal kedaluwarsa 30 hari dari saat ini
             Cookies.set('token', token, { expires: expirationDate });
+        },
+        togglePasswordVisibility() {
+            this.showPassword = !this.showPassword;
         }
     },
     mounted() {
@@ -73,7 +77,8 @@ export default {
 
 <template>
     <!-- component -->
-    <div class="bg-sign md:bg-none md:h-full h-screen md:bg-white bg-transparentblack bg-blend-darken md:bg-blend-normal flex flex-row-reverse ">
+    <div
+        class="bg-sign md:bg-none md:h-full h-screen md:bg-white bg-transparentblack bg-blend-darken md:bg-blend-normal flex flex-row-reverse ">
         <!-- Left: Image -->
         <div class="w-1/2 hidden lg:block">
             <router-link to="/"><img src="../assets/sign.png" alt="Placeholder Image"
@@ -88,7 +93,8 @@ export default {
                 <!-- Email Input -->
                 <div class="mb-4">
                     <label for="Email"
-                        class="block font-[verdana] text-white md:text-black font-normal text-[14px] text-[#344054] mb-2">Email atau Nomor HP</label>
+                        class="block font-[verdana] text-white md:text-black font-normal text-[14px] text-[#344054] mb-2">Email
+                        atau Nomor HP</label>
                     <input type="text" id="Email" name="Email" v-model="EmailOrPhoneNumber" required
                         class="w-full border border-lightgrayish rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
                         autocomplete="off" placeholder="enter your email or phone number">
@@ -96,24 +102,52 @@ export default {
                 <!-- Password Input -->
                 <div class="mb-4">
                     <label for="password" class="block text-[#344054] mb-2 text-white md:text-black">Password</label>
-                    <input type="password" id="password" name="password" v-model="password" required
-                        class="w-full border border-lightgrayish rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
-                        autocomplete="off" placeholder="enter password">
+                    <div class="relative">
+                        <input :type="showPassword ? 'text' : 'password'" id="password" name="password" v-model="password"
+                            required
+                            class="w-full border border-lightgrayish rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+                            autocomplete="off" placeholder="enter password">
+                        <button type="button" @click="togglePasswordVisibility"
+                            class="absolute inset-y-0 right-0 px-3 flex items-center text-gray-500 focus:outline-none">
+                            <span v-if="showPassword"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M2 2L22 22" stroke="#000000" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" />
+                                    <path
+                                        d="M6.71277 6.7226C3.66479 8.79527 2 12 2 12C2 12 5.63636 19 12 19C14.0503 19 15.8174 18.2734 17.2711 17.2884M11 5.05822C11.3254 5.02013 11.6588 5 12 5C18.3636 5 22 12 22 12C22 12 21.3082 13.3317 20 14.8335"
+                                        stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path
+                                        d="M14 14.2362C13.4692 14.7112 12.7684 15.0001 12 15.0001C10.3431 15.0001 9 13.657 9 12.0001C9 11.1764 9.33193 10.4303 9.86932 9.88818"
+                                        stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg></span>
+                            <span v-else><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12" stroke="#000000" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M1 12C1 12 5 20 12 20C19 20 23 12 23 12" stroke="#000000" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                    <circle cx="12" cy="12" r="3" stroke="#000000" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" />
+                                </svg></span>
+                        </button>
+                    </div>
                 </div>
                 <!-- Remember Me Checkbox -->
                 <div class="mb-4 flex flex-col">
                     <div>
                         <input type="checkbox" id="remember" name="remember" class="outline-[#D4A02C]">
-                        <label for="remember" class="font-[verdana] md:text-[#344054] text-white font-normal text[14px] ml-2">Remember
+                        <label for="remember"
+                            class="font-[verdana] md:text-[#344054] text-white font-normal text[14px] ml-2">Remember
                             Me</label>
                     </div>
-                    <p class="pl-5 font-[verdana] font-normal text[14px] text-white md:text-[#667085]">Save my login details for next
+                    <p class="pl-5 font-[verdana] font-normal text[14px] text-white md:text-[#667085]">Save my login details
+                        for next
                         time.</p>
                 </div>
                 <!-- Login Button -->
                 <div class="flex items-center flex-col">
-                    <button type="submit"
-                        class="bg-teal text-white font-semibold rounded-md py-2 px-4 w-full">Login</button>
+                    <button type="submit" class="bg-teal text-white font-semibold rounded-md py-2 px-4 w-full">Log
+                        in</button>
                     <div class="mt-6 text-blue-500">
                         <router-link to="/lupapassword">
                             <a target="_blank"
@@ -123,8 +157,7 @@ export default {
                     </div>
                 </div>
 
-            </form>
-            <!-- Forgot Password Link -->
-        </div>
+        </form>
+        <!-- Forgot Password Link -->
     </div>
-</template>
+</div></template>
