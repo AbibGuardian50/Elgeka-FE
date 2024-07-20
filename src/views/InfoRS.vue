fd387a381bbc86ea5e3cfc5b5ec725608965de36
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2024-07-20 10:14:26 +0700

    Height

--- src/views/InfoRS.vue
+++ src/views/InfoRS.vue
@@ -9,7 +9,7 @@
 
         <div class="grid justify-center absolute max-md:top-[72rem] max-sm:relative max-sm:top-[93rem] max-sm:overflow-x-hidden  max-[1200px]:top-[61rem] min-[1200px]:top-[50rem] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 min-[1200px]:grid-cols-4 gap-4 place-items-center min-w-[320px] sm:min-w-[640px] md:min-w-[880px] lg:min-w-[1011px] min-[1200px]:min-w-[1200px] max-w-[1260px] bg-gray-100">
-                <div v-for="data in paginatedInfoRS" :key="data.id" class="bg-verylightgray rounded-lg shadow-lg overflow-hidden w-[291px]  flex flex-col justify-between">
+                <div v-for="data in paginatedInfoRS" :key="data.id" class="bg-verylightgray rounded-lg shadow-lg overflow-hidden w-[291px] h-[24.375rem] flex flex-col justify-between">
                     <div class="flex items-center justify-center pt-4 px-2">
                         <img :src="data.image_url ? url + data.image_url : 'https://cdn.pixabay.com/photo/2021/11/20/03/16/doctor-6810750_1280.png'" alt="foto rumah sakit" class="w-full max-w-[262px] rounded-[1rem] max-h-[144px] object-cover">
                     </div>
